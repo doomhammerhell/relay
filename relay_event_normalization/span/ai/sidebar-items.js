46b369b3913cbb8b281bfacce9ea508a92ecd6a8
doomhammerhell <doomhammerhell@users.noreply.github.com>
2026-06-27 19:33:04 +0000

    deploy: d127d3c74e5b97a0ebc0b81e48bd96d4732280a5

---
+++ relay_event_normalization/span/ai/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["DEFAULT_AI_OPERATION"],"fn":["calculate_costs","enrich_ai_event_data","enrich_ai_span","infer_ai_operation_type"],"struct":["CalculatedCost","UsedTokens"]};
